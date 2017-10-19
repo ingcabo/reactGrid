1f3ff2612c4b410235b62f18838d67f92a71f909
ingcabo <ingcabo@gmail.com>
2017-10-19 00:23:42 -0300

    react-router-redux

--- app/src/components/ProductList/ProductList.js
+++ app/src/components/ProductList/ProductList.js
@@ -1,8 +1,7 @@
 import React  from 'react';
 import { connect } from 'react-redux';
-//import React, { Component } from 'react';
-import { Button, Glyphicon } from 'react-bootstrap';
-//import store from '../store.js';
+import { push } from 'react-router-redux';
+import { Button, Glyphicon, Table, Pagination } from 'react-bootstrap';
 import {  addToCart } from '../../actions/actionCreators.js';
 
 
@@ -19,28 +18,52 @@ const styles = {
   }
 };
 
-const ProductList = ({ products, addToCart }) => {
+const ProductList = ({ products, addToCart, page, changePage}) => {
+
+  // pagination
+
+const per_page = 5;
+const pages = Math.ceil(products.length / per_page);
+const start_offset = (page - 1) * per_page;
+let start_count = 0;
+
+
   return (
+<div>
     <div style={styles.products}>
-      {products.map(product =>
-        <div className="thumbnail" style={styles.product} key={product.id}>
-          <img src={product.image} alt={product.name} />
-          <div className="caption">
-            <h4>{product.name}</h4>
-            <p>
-              <Button bsStyle="primary" onClick={() => addToCart(product)} role="button" disabled={product.inventory <= 0}>${product.price} <Glyphicon glyph="shopping-cart" /></Button>
-            </p>
-          </div>
-        </div>
-      )}
+        {products.map((product,index) => {
+            if (index >= start_offset && start_count < per_page) {
+              start_count++;
+              return(
+              <div className="thumbnail" style={styles.product} key={product.id}>
+                <img src={product.image} alt={product.name} />
+                <div className="caption">
+                  <h4>{product.name}</h4>
+                  <p>
+                    <Button bsStyle="primary" onClick={() => addToCart(product)} role="button" disabled={product.inventory <= 0}>${product.price} <Glyphicon glyph="shopping-cart" /></Button>
+                  </p>
+                </div>
+              </div>
+            );
+           }
+        })}
     </div>
+    <Pagination className="products-pagination pull-right" bsSize="medium" maxButtons={10} first last next
+    prev boundaryLinks items={pages} activePage={page} onSelect={changePage}/>
+</div>
   );
 };
+
+// change the user lists' current page
+const changePage = (page) => {
+  this.props.dispatch(push('/?page=' + page));
+}
 
 
 const mapStateToProps = (state) => {
   return {
-    products: state.products
+    products: state.products,
+     page: Number(1) || 1,
   };
 };
 
@@ -51,5 +74,6 @@ const mapDispatchToProps = (dispatch) => {
     }
   };
 }
-
+//https://github.com/catalin-luntraru/redux-minimal/blob/master/src_users/components/common/UserList.js
+//https://www.youtube.com/watch?v=2qxNVzmiR8Y
 export default connect(mapStateToProps, mapDispatchToProps)(ProductList);
