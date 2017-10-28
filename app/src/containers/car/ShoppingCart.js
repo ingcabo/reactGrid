0f7c19d5abbeda6fb901267490e3886286862954
ingcabo <ingcabo@gmail.com>
2017-10-28 01:21:14 -0300

    Revert "Revert "Revert "Revert "Fix de carrito y listado""""
    This reverts commit a39e4859d375971311343fc96de12b36dbb2b6a7.

--- app/src/containers/car/ShoppingCart.js
+++ app/src/containers/car/ShoppingCart.js
@@ -1,16 +1,8 @@
-import React, { Component } from 'react';
-import { Panel, Table, Button, Glyphicon } from 'react-bootstrap';
+import React, {Component} from 'react';
+import {Panel, Table, Button, Glyphicon} from 'react-bootstrap';
 //import store from '../../lib/store.js';
-import {  removeFromCart } from './actions.js';
-import { connect } from 'react-redux';
-
-//import { removeCart } from '../containers/car/actions.js';
-
-// Redux store
-import configureStore from '../../lib/configureStore.js';
-
-// Configuring redux store
-const store = configureStore();
+import {removeFromCart} from './actions.js';
+import {connect} from 'react-redux';
 
 
 const styles = {
@@ -20,63 +12,54 @@ const styles = {
 }
 
 class ShoppingCart extends Component {
-
-/*ya utilizamos react react-redux y no neceiso en constructor se utiliza mapStateToProps para llevar el estado inicial */
-
-  constructor() {
-    super();
-      //actualizar el estado local
-      store.subscribe(()=>{
-       //actualizar el estado local
-       this.setState({
-         cart: store.getState().cart
-       });
-     });
+  /*ya utilizamos react react-redux y no neceiso en constructor se utiliza mapStateToProps para llevar el estado inicial */
+  constructor(props) {
+    super(props);
   }
 
   render() {
-    //  console.log(this.props.cart); {Math.random( 100, 10)
+    const {cart} = this.props
     return (
       <Panel header="Promo Cart">
-
         <Table fill>
           <tbody>
-            {this.props.cart.map(product =>
-              <tr key={product.id}>
-                <td>{product.name}</td>
-                <td>{product.cant}</td>
-                <td className="text-right">${product.price}</td>
-                <td className="text-right">${product.price * product.cant}</td>
-                <td className="text-right"><Button bsSize="xsmall" bsStyle="danger" onClick={() => this.props.removeFromCart(product)}><Glyphicon glyph="trash" /></Button></td>
-              </tr>
-            )}
+          {cart.map(product =>
+            <tr key={product.id}>
+              <td>{product.name}</td>
+              <td>{product.cant}</td>
+              <td className="text-right">${product.price}</td>
+              <td className="text-right">${product.price * product.cant}</td>
+              <td className="text-right"><Button bsSize="xsmall" bsStyle="danger"
+                                                 onClick={() => this.props.removeFromCart(product)}><Glyphicon
+                glyph="trash"/></Button></td>
+            </tr>
+          )}
           </tbody>
           <tfoot>
-            <tr>
-              <td colSpan="4" style={styles.footer}>
-                Total: ${this.props.cart.reduce((sum, product) => sum + product.price * product.cant, 0)}
-              </td>
-            </tr>
+          <tr>
+            <td colSpan="4" style={styles.footer}>
+              Total: ${this.props.cart.reduce((sum, product) => sum + product.price * product.cant, 0)}
+            </td>
+          </tr>
           </tfoot>
         </Table>
-
       </Panel>
     )
   }
 }
 
-const mapStateToProps = (state) =>{
+const mapStateToProps = (state) => {
   return {
-    cart: state.cart
+    cart: state.cart.cart
   };
 };
 
-const mapDispatchToProps = dispatch =>{
-  return{
+const mapDispatchToProps = dispatch => {
+  return {
     removeFromCart(product){
       dispatch(removeFromCart(product));
     }
   };
 };
 
-export default connect(mapStateToProps,mapDispatchToProps)(ShoppingCart);
+export default connect(mapStateToProps, mapDispatchToProps)(ShoppingCart);
--- app/src/containers/car/reducer.js
+++ app/src/containers/car/reducer.js
@@ -1,39 +1,48 @@
-
 //definimos la funcion reductora recive dos parametros el estado actual y la accion
-var cars = new Array();
-
-
-export const initialStateCart = {
-  cart:[]
-  }
-
-
+export const initialStateCart = {cart: []}
 //funcion que resta items del carrito
-const restItems = (state, action) => {
+const restItems = (cars, action) => {
 
-    cars = state;
-    const index = cars.findIndex(item => item.id === action.product.id);
-    if (action.product.cant == 1) {
-        //buscar el indice por medio de un valor de campo
-        cars.splice(index, 1);
+  //cars.filter()
+
+  const index = cars.findIndex(item => item.id === action.product.id);
+  if (action.product.cant == 1) {
+    //buscar el indice por medio de un valor de campo
+    cars.splice(index, 1);
+  } else {
+    cars[index]['cant']--;
+  }
+  return cars;
+}
+
+//funcion para sumar item al carrito de compra
+const AddItemCount = (cars, action) => {
+  const longArray = cars.length;
+  const index = cars.findIndex(item => item.id === action.product.id);
+  if (longArray == 0) {
+    cars = cars.concat(action.product);
+    cars[longArray]['cant'] = 1
+  } else {
+    if (index == -1) {
+      cars = cars.concat(action.product);
+      cars[longArray]['cant'] = 1
     } else {
-        cars[index]['cant'] --;
+      cars[index]['cant']++;
     }
-    return cars;
+  }
+  return cars;
 }
 
 
-export default function  CartReducer  (state = [], action) {
+export default function CartReducer(state = initialStateCart, action) {
 
-    if (action.type === "REMOVE_FROM_CART") {
-
-        cars = restItems(state.cart, action);
-
-        return {
-            ...state,
-            cart: cars
-        };
-    }
-
-    return state;
-};
+  if (action.type === "REMOVE_FROM_CART") {
+    let cars = restItems([...state.cart], action);
+    return {...state, cart: cars}
+  } else if (action.type === "ADD_TO_CART") {
+    let cars = AddItemCount([...state.cart], action);
+    return Object.assign({}, state, {cart:cars})
+    //return {...state, cart: cars};
+  }
+  return state;
+}
